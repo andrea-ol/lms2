25e83270641d966acdba475be11a95ded05897b4
andrea-ol <163352071+andrea-ol@users.noreply.github.com>
2024-03-27 16:25:25 -0500

    new setting

--- db_config.php
+++ db_config.php
@@ -2,12 +2,12 @@
     /*
     CONFIGURACION DE CONEXION A LA BASE DE DATOS LMS
     */
-    $password = "1234";
+    $password = "12345";
     $usuario = "postgres";
     $nombreBaseDeDatos = "zajuna";
     # Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
     $rutaServidor = "localhost";
-    $puerto = 5432;
+    $puerto = 5433;
     try {
         $conn = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
