78028e9d8d26101be74502d7623c731c295567de
andrea-ol <163352071+andrea-ol@users.noreply.github.com>
2024-03-27 15:03:34 -0500

    new

--- actualizar_acti.php
+++ actualizar_acti.php
@@ -3,23 +3,22 @@ require_once 'db_config.php';
 //$idcurso=$_GET['id'];
 
 // SE RECIBEN LOS DATOS ENVIADOS POR EL FORMULARIO
-$user = $_POST['user'];  
-$cali_depro = $_POST['edit_calificacion2']; // CALIFICACION DEPROBADA
-$cali_pend = $_POST['edit_calificacion3']; // CALIFICACION PENDIENTESS
+$user = $_POST['user_id'];  
+$cali = $_POST['edit_calificacion2']; // CALIFICACION DEPROBADA
 $acti= $_POST['actividad']; // ACTIVIDAD A CALIFICAR
- // EdDITAR NOTA APROBADA
+
+$curso = $_POST['curso_id'];
+$idcurso = $curso[0];
 
 // SE RECORRE ARREGLO DE DATOS A TRAVES DE UN FOR PARA LUEGO REALIZAR EL UPDATE EN LA BD
 for ($i = 0; $i < count($user); $i++) {
-    $update_query = $conn->query("UPDATE mdl_quiz_grades SET grade = '$cali_depro[$i]' WHERE userid = '$user[$i]' AND quiz = '$acti[$i]'");
+    $update_query = $conn->query("UPDATE mdl_quiz_grades SET grade = '$cali[$i]' WHERE userid = '$user[$i]' AND quiz = '$acti[$i]'");
 }
-
 
 
 // mensaje actualización exitosa
 $mensaje = "LA ACTUALIZACIÓN DEL CENTRO DE CALIFICACIONES SE HA REALIZADO CON ÉXITO";
         echo "<script type='text/javascript'>
         alert('$mensaje');
-        window.location.href = 'resultados.php';
+        window.location.href = 'actividades.php?id_curso=${idcurso}';
         </script>";
-
